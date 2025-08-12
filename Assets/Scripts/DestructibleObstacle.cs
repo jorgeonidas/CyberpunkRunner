0aa20639533ce355a802dc93949c84a8149f15b0
Jorgeonidas <zangetsujm@gmail.com>
2025-08-12 11:38:05 -0300

    prefab to display car destroyed

--- Assets/Scripts/DestructibleObstacle.cs
+++ Assets/Scripts/DestructibleObstacle.cs
@@ -4,6 +4,8 @@ public class DestructibleObstacle : MonoBehaviour, IDestroy
 {
     VFXSpawner _vfxSpawner;
     PooledObject _poolObject;
+    //test
+    [SerializeField] Rigidbody _destroyectVisuals;
     void Awake()
     {
         if (TryGetComponent<PooledObject>(out PooledObject poolObject))
@@ -19,6 +21,19 @@ public class DestructibleObstacle : MonoBehaviour, IDestroy
     public void DestroyMe()
     {
         //play vfx spawn destroyed vehicle
+        Rigidbody rigidbody = Instantiate(_destroyectVisuals, transform.position, transform.rotation);
+        Vector3 upForce = Vector3.up * Random.Range(5, 10);
+        Vector3 backForce = Vector3.forward * Random.Range(-5, 5);
+        Vector3 sideForce = Random.value < 0.5f ? Vector3.right * 10 : Vector3.left * 10;
+        rigidbody.AddForce(upForce + backForce + sideForce, ForceMode.Impulse);
+        // Torque aleatorio
+        float torque = 5f;
+        Vector3 randomTorque = new Vector3(
+            Random.Range(-torque, torque),
+            Random.Range(-torque, torque),
+            Random.Range(-torque, torque)
+        );
+        rigidbody.AddTorque(randomTorque, ForceMode.Impulse);
         _vfxSpawner?.PlayParticleEffect(transform.position);
         _poolObject?.Release();
     }
