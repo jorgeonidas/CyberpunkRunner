ee1709e5d3efdc4e136d4a644405e05546c49a38
Jorgeonidas <zangetsujm@gmail.com>
2025-08-15 13:06:06 -0300

    needle for the thacometer

--- Assets/Scripts/UI/TachometerUI.cs
+++ Assets/Scripts/UI/TachometerUI.cs
@@ -5,23 +5,26 @@ using UnityEngine.UI;
 public class TachometerUI : MonoBehaviour
 {
     [Header("UI Elements")]
-    [SerializeField] private Slider _progressBar;
-    [SerializeField] private Image _fillImage;
+    [SerializeField] private RectTransform needleTransform;
+    //[SerializeField] private Image _fillImage;
     [SerializeField] private Gradient _colorGradient;
     [SerializeField] private TextMeshProUGUI _currentSpeedText;
 
     [Header("Speed Settings")]
-    [SerializeField] private float smoothSpeed = 5f;
-    [SerializeField] private SpeedManager speedManager;
+    [SerializeField] private float _smoothSpeed = 5f;
+    [SerializeField] private SpeedManager _speedManager;
+    [Header("Needle Settings")]
+    [SerializeField] private float _minAngle = -90f; // Ángulo mínimo de la aguja
+    [SerializeField] private float _maxAngle = 90f;  // Ángulo máximo de la aguja
 
     private float currentSpeed;
     private float _maxSpeed = 100f;
 
     private void Start()
     {
-        if (speedManager != null)
+        if (_speedManager != null)
         {
-            _maxSpeed = speedManager.MaxChunkSpeed * 2;
+            _maxSpeed = _speedManager.MaxChunkSpeed;
         }
     }
 
@@ -32,16 +35,16 @@ public class TachometerUI : MonoBehaviour
 
     private void UpdateSpeed()
     {
-        float targetSpeed = speedManager?.CurrentChunksMoveSpeed ?? 0;
+        float targetSpeed = _speedManager?.CurrentChunksMoveSpeed ?? 0;
 
-        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, smoothSpeed * Time.deltaTime);
+        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, _smoothSpeed * Time.deltaTime);
 
-        float fillAmount = Mathf.Clamp01(currentSpeed / _maxSpeed);
+        float normalizedSpeed = Mathf.Clamp01(currentSpeed / _maxSpeed);
 
-        _progressBar.value = fillAmount;
+        float targetAngle = Mathf.Lerp(_minAngle, _maxAngle, normalizedSpeed);
 
-        _fillImage.color = _colorGradient.Evaluate(fillAmount);
-
+        needleTransform.localRotation = Quaternion.Euler(0f, 0f, targetAngle);
         _currentSpeedText.text = $"{(currentSpeed * 10).ToString("f0")} Km/h";
+        _currentSpeedText.color = _colorGradient.Evaluate(normalizedSpeed);
     }
 }
