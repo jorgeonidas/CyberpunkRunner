79081822942eed1f20777144bdf43dbc4d61b3d0
Jorgeonidas <zangetsujm@gmail.com>
2025-08-28 14:13:11 -0300

    Checkpoint: Shop buttons

--- Assets/Scripts/UI/Shop/ShopPanel.cs
+++ Assets/Scripts/UI/Shop/ShopPanel.cs
@@ -1,10 +1,16 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopPanel : AbstractUIPanel
 {
     public static Action<string, ProductCategory> OnItemSelected;
     public override string Id => StringConstants.MainMenuPanels.ShopMenu;
+    [Header("Shop Elements")]
+    [SerializeField] Button _equipButton;
+    [SerializeField] GameObject _equippedLabel;
+    [SerializeField] Button _purchaseButton;
+    //[SerializeField] Ted _purchaseButton;
     [SerializeField] ShopView _paintShop;
 
     public override void Show()
