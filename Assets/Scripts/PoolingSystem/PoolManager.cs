abab81c948ef048f83391807fe32c394a0c941ce
Jorgeonidas <zangetsujm@gmail.com>
2025-09-08 14:02:14 -0300

    register to pool function

--- Assets/Scripts/PoolingSystem/PoolManager.cs
+++ Assets/Scripts/PoolingSystem/PoolManager.cs
@@ -25,48 +25,89 @@ public class PoolManager : MonoBehaviour
     {
         foreach (var configuration in _poolObjectConfigs)
         {
-            ObjectPool<PooledObject> pool = null;
+            RegisterPool(configuration.Config.key,
+                        configuration.Config.objectPrefab,
+                        configuration.Config.defaultCapacity,
+                        configuration.Config.maxSize,
+                        configuration.Config.prewarm);
+            // ObjectPool<PooledObject> pool = null;
 
-            pool = new ObjectPool<PooledObject>(
-                createFunc: () =>
-                {
-                    PooledObject poolObject = Instantiate(configuration.Config.objectPrefab);
-                    poolObject.Pool ??= pool;     // ?? assing if not assigned
-                    poolObject.SetPoolObjectId(configuration.Config.key);
-                    poolObject.transform.parent = this.transform;
-                    poolObject.gameObject.SetActive(false);
-                    return poolObject;
-                },
-                actionOnGet: (poolObject) =>
-                {
-                    poolObject.gameObject.SetActive(true);
-                    poolObject.OnGetFromPool();
-                },
-                actionOnRelease: (poolObject) =>
-                {
-                    poolObject.OnReleaseToPool();
-                    poolObject.gameObject.SetActive(false);
-                },
-                actionOnDestroy: (poolObject) =>
-                {
-                    if (poolObject)
-                    {
-                        Destroy(poolObject.gameObject);
-                    }
-                },
-                collectionCheck: true,
-                defaultCapacity: Mathf.Max(1, configuration.Config.defaultCapacity),
-                maxSize: Mathf.Max(1, configuration.Config.maxSize)
-            );
+            // pool = new ObjectPool<PooledObject>(
+            //     createFunc: () =>
+            //     {
+            //         PooledObject poolObject = Instantiate(configuration.Config.objectPrefab);
+            //         poolObject.Pool ??= pool;     // ?? assing if not assigned
+            //         poolObject.SetPoolObjectId(configuration.Config.key);
+            //         poolObject.transform.parent = this.transform;
+            //         poolObject.gameObject.SetActive(false);
+            //         return poolObject;
+            //     },
+            //     actionOnGet: (poolObject) =>
+            //     {
+            //         poolObject.gameObject.SetActive(true);
+            //         poolObject.OnGetFromPool();
+            //     },
+            //     actionOnRelease: (poolObject) =>
+            //     {
+            //         poolObject.OnReleaseToPool();
+            //         poolObject.gameObject.SetActive(false);
+            //     },
+            //     actionOnDestroy: (poolObject) =>
+            //     {
+            //         if (poolObject)
+            //         {
+            //             Destroy(poolObject.gameObject);
+            //         }
+            //     },
+            //     collectionCheck: true,
+            //     defaultCapacity: Mathf.Max(1, configuration.Config.defaultCapacity),
+            //     maxSize: Mathf.Max(1, configuration.Config.maxSize)
+            // );
 
-            _pools[configuration.Config.key] = pool;
+            // _pools[configuration.Config.key] = pool;
 
-            // Precalienta si quieres
-            for (int i = 0; i < configuration.Config.prewarm; i++)
+            // // Precalienta si quieres
+            // for (int i = 0; i < configuration.Config.prewarm; i++)
+            // {
+            //     var o = pool.Get();
+            //     pool.Release(o);
+            // }
+        }
+    }
+
+    public void RegisterPool(string key, PooledObject prefab, int defaultCapacity, int maxSize, int prewarm, Transform parent = null)
+    {
+        if (_pools.ContainsKey(key))
+        {
+            return;
+        }
+
+        ObjectPool<PooledObject> pool = null;
+        pool = new ObjectPool<PooledObject>(
+            createFunc: () =>
             {
-                var o = pool.Get();
-                pool.Release(o);
-            }
+                var obj = Instantiate(prefab);
+                if (obj.Pool == null) obj.Pool = pool;
+                obj.SetPoolObjectId(key);
+                obj.transform.parent = parent != null ? parent : transform;
+                obj.gameObject.SetActive(false);
+                return obj;
+            },
+            actionOnGet: o => { o.gameObject.SetActive(true); o.OnGetFromPool(); },
+            actionOnRelease: o => { o.OnReleaseToPool(); o.gameObject.SetActive(false); },
+            actionOnDestroy: o => { if (o) Destroy(o.gameObject); },
+            collectionCheck: true,
+            defaultCapacity: Mathf.Max(1, defaultCapacity),
+            maxSize: Mathf.Max(1, maxSize)
+        );
+
+        _pools[key] = pool;
+
+        // Prewarm
+        for (int i = 0; i < prewarm; i++)
+        {
+            var o = pool.Get();
+            pool.Release(o);
         }
     }
 
